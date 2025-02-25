d2a96d373a9904fbbc9a97e93eb6aafd3d31fd40
Kath-daiane <kath.daiane.p@gmail.com>
2025-02-25 11:16:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        Console.Write("Digite o preço do produto: ");
+        double preco = double.Parse(Console.ReadLine());
+        double desconto = preco * 0.15;
+        double PrecoComDesconto = preco - desconto;
+
+        Console.WriteLine($"Preço original R$ {preco}, com desconto fica R$ {PrecoComDesconto}");
+          
+    }
+}
